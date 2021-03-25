dfa0e846f9d1a2d943ef796fa87b30b8d6210dc6
CSIDanielD <daniel.durbin@csiweb.com>
2021-03-24 21:00:08 -0500

    moved registering actions out to their own functions; I don't like this syntax, and I'd prefer it to be where people can write very simple "reducer" functions for each action. I'll work on it.

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,46 +1,7 @@
 import { Injectable } from "@angular/core";
+import { StoreService } from "./store.service";
 
 @Injectable({ providedIn: "root" })
 export class NoteService {
-  /**
-   * At the top of any service that will be modifying global state (i.e. dispatching actions),
-   * register your actions like:
-   *
-   *    const actions = {
-   *     'addNote': (state, note) => state.notes.push(note),
-   *     'removeNote': (state, note) => state.notes.find(note).remove()  // I know this isn't real syntax
-   *    };
-   *
-   * Then pass it into some registration function like:
-   *
-   *     // 'selector' is the selector used to get the state we want for the ActionReducers
-   *     // It's optional - if not present, we'll pass in the whole state.
-   *     store.registerActions(actions, selector?);
-   *
-   * This will automatically append the actions by key to the global ActionReducers map in the Store:
-   *
-   * const actionReducers : {[actionKey: string]: Array<ActionReducer>} = {
-   *  'addNote': ActionReducer1, ActionReducer2, etc...
-   *  'removeNote': ActionReducer3, ActionReducer4, etc...
-   * }
-   *
-   * Then in the service, we dispatch one of our actions...
-   *
-   * ...which we subscribe to (listen for) in the StoreService. It will look up that action by
-   * key, and run ALL the ActionReducers for that action:
-   *
-   * onActionDispatched(action: Action) {
-   *  const reducers = this.actionReducers[action.key];
-   *  for (const reducer of reducers) {
-   *
-   *    // Execute the selector to get the appropriate state to pass in to the reducer..
-   *    const selectedState = reducer.selector(this._appState.value);
-   *
-   *    // Execute the ActionReducer
-   *    reducer.execute()
-   *  }
-   * }
-   */
-
-  constructor() {}
+  constructor(private store: StoreService) {}
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { FakeBackendService } from "./fake-backend.service";
 import { StoreService } from "./store.service";
 import UserTask from "../types/userTask";
@@ -9,26 +9,40 @@ export class TaskService {
     private store: StoreService,
     private fakeBackend: FakeBackendService
   ) {
+    this.registerGetTasks();
+    this.registerAddTask();
+    this.registerUpdateTask();
+    this.registerRemoveTask();
+
+    this.registerMakeFakeTasks([
+      { taskId: 1, name: "Fake Task 1", difficulty: 20, progress: 0.4 },
+      { taskId: 1, name: "Fake Task 2", difficulty: 3, progress: 0.9 }
+    ]);
+  }
+
+  private registerGetTasks() {
     this.store.registerAction<UserTask[]>("getTasks", async s => {
       const tasks = await this.fakeBackend.getTasks().toPromise();
       s.tasks = tasks;
       return s;
     });
+  }
 
+  private registerMakeFakeTasks(fakeTasks: UserTask[] = []) {
     this.store.registerAction("makeFakeTasks", s => {
-      s.tasks = [
-        { taskId: 1, name: "Fake Task 1", difficulty: 20, progress: 0.4 },
-        { taskId: 1, name: "Fake Task 2", difficulty: 3, progress: 0.9 }
-      ];
-
+      s.tasks = fakeTasks;
       return s;
     });
+  }
 
+  private registerAddTask() {
     this.store.registerAction<UserTask>("addTask", (s, task) => {
       s.tasks.push(task);
       return s;
     });
+  }
 
+  private registerUpdateTask() {
     this.store.registerAction("updateTask", (s, task) => {
       const index = s.tasks.findIndex(task);
 
@@ -38,7 +52,9 @@ export class TaskService {
 
       return s;
     });
+  }
 
+  private registerRemoveTask() {
     this.store.registerAction("removeTask");
   }
 }
