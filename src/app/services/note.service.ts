54e2b9e5a64909d9b06bd01f1160ccaf404aea35
CSIDanielD <daniel.durbin@csiweb.com>
2021-04-06 14:26:43 -0500

    Removed references to old store

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,25 +1,9 @@
 import { Injectable } from "@angular/core";
-import { FakeBackendService } from "./fake-backend.service";
-import { StoreService } from "./store.service";
-import { AppState } from "../types/appState";
-import { ActionReducer } from "../types/basic-store/actionReducer";
-import { ActionContext } from "../types/basic-store/actionContext";
 
 @Injectable({ providedIn: "root" })
 export class NoteService {
   constructor(
     private store: StoreService,
     private fakeBackend: FakeBackendService
-  ) {
-    const context = new ActionContext(this.store, this);
-    context.registerAction("getNotes", this.getNotes);
-  }
-
-  private getNotes: ActionReducer<AppState> = async (action, getState) => {
-    const notes = await this.fakeBackend.getNotes().toPromise();
-
-    const s = this.store.select(s => s);
-    s.notes = notes;
-    return s;
-  };
+  ) {}
 }
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { skip } from "rxjs/operators";
 import { AppState } from "../types/appState";
-import { BasicStore } from "../types/basic-store/basicStore";
 
 @Injectable({ providedIn: "root" })
-export class StoreService extends BasicStore<AppState> {
+export class StoreService {
   static readonly defaultState: AppState = {
     users: [],
     notes: [],
@@ -18,18 +17,12 @@ export class StoreService extends BasicStore<AppState> {
       }
     ]
   };
+  
 
   private _stateUpdateCount = new BehaviorSubject<number>(0);
   get stateUpdateCount() {
     return this._stateUpdateCount.value;
   }
 
-  constructor() {
-    super(StoreService.defaultState);
-
-    const state$ = this.selectAsync(s => s);
-    state$.pipe(skip(1)).subscribe(s => {
-      this._stateUpdateCount.next(this._stateUpdateCount.value + 1);
-    });
-  }
+  constructor() {}
 }
