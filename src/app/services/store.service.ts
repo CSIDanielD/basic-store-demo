4773affe76039186ffba65770141fd9612474158
CSIDanielD <daniel.durbin@csiweb.com>
2021-04-06 15:21:19 -0500

    hooked up the state update counter again

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { skip } from "rxjs/operators";
+import { BasicStore } from "../basic-store/basicStore";
 import { AppState } from "../types/appState";
 import { NoteService } from "./note.service";
 import { UserService } from "./user.service";
 
 @Injectable({ providedIn: "root" })
 export class StoreService {
-  static readonly defaultState: AppState = {
+  static readonly defaultState: Readonly<AppState> = {
     users: [],
     notes: [],
     tasks: [
@@ -20,6 +21,8 @@ export class StoreService {
     ]
   };
 
+  store = new BasicStore(StoreService.defaultState, {});
+
   private _stateUpdateCount = new BehaviorSubject<number>(0);
   get stateUpdateCount() {
     return this._stateUpdateCount.value;
@@ -28,5 +31,14 @@ export class StoreService {
   constructor(
     private userService: UserService,
     private noteService: NoteService
-  ) {}
+  ) {
+
+    // Increment the state update counter when the state changes.
+    this.store
+      .selectAsync(s => s)
+      .pipe(skip(1))
+      .subscribe(() =>
+        this._stateUpdateCount.next(this._stateUpdateCount.value + 1)
+      );
+  }
 }
