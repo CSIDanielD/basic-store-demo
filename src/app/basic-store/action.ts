faefb28ce4cdba523cc651bcc41ba2d742a260b4
CSIDanielD <daniel.durbin@csiweb.com>
2021-04-07 07:43:30 -0500

    immediately figured out a simple solution to differentiate ActionCreators

--- src/app/basic-store/action.ts
+++ src/app/basic-store/action.ts
@@ -1,8 +1,15 @@
+import { IsUnknown } from "./utilityTypes";
+
 export interface Action<P = any> {
   type: string;
   payload: P;
 }
 
-// TODO: Find a way to make payload required without mandating non-payload actions to provide an argument.
-export type ActionCreator<P> = (payload?: P) => Action<P>;
+
+export type ActionCreator<P> = IsUnknown<
+  P,
+  ActionCreatorWithoutPayload,
+  ActionCreatorWithPayload<P>
+>;
+export type ActionCreatorWithPayload<P> = (payload: P) => Action<P>;
 export type ActionCreatorWithoutPayload = () => Action<undefined>;
